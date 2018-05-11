e7d30e139b7c5116f5d2b5d068c6ac6c258febed
Karl Henrik Elg <klink100@gmail.com>
2018-05-11 20:27:53 +0200

    Change background color to cyan

--- core/src/no/erlyberly/bootlegterraria/GameMain.java
+++ core/src/no/erlyberly/bootlegterraria/GameMain.java
@@ -42,7 +42,7 @@ public class GameMain extends Game {
 	@Override
 	public void render () {
 
-	    Gdx.gl.glClearColor(1,1,1,1);
+	    Gdx.gl.glClearColor(0,1,1,1);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if(Gdx.input.justTouched()){
