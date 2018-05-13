94c7f35f04f8020e931a16db42592e55b10f1d61
Karl Henrik Elg <klink100@gmail.com>
2018-05-13 15:47:28 +0200

    Moar FPS

--- desktop/src/no/erlyberly/bootlegterraria/desktop/DesktopLauncher.java
+++ desktop/src/no/erlyberly/bootlegterraria/desktop/DesktopLauncher.java
@@ -2,16 +2,21 @@ package no.erlyberly.bootlegterraria.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import no.erlyberly.bootlegterraria.helpers.GameInfo;
 import no.erlyberly.bootlegterraria.GameMain;
+import no.erlyberly.bootlegterraria.helpers.GameInfo;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.width = GameInfo.WIDTH;
-		config.height = GameInfo.HEIGHT;
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		new LwjglApplication(new GameMain(), config);
-	}
+        config.width = GameInfo.WIDTH;
+        config.height = GameInfo.HEIGHT;
+        config.vSyncEnabled = false;
+        config.foregroundFPS = 9999;
+        config.backgroundFPS = 10;
+        config.samples = 16;
+
+        new LwjglApplication(new GameMain(), config);
+    }
 }
