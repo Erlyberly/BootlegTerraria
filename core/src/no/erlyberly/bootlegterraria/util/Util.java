8e82f78095ff7b6ec52ac1f04e63325209569985
Karl Henrik Elg <klink100@gmail.com>
2018-05-26 14:23:16 +0200

    Add utility methods for checking and setting values between two other

--- core/src/no/erlyberly/bootlegterraria/util/Util.java
+++ core/src/no/erlyberly/bootlegterraria/util/Util.java
@@ -1,6 +1,7 @@
 package no.erlyberly.bootlegterraria.util;
 
 import com.badlogic.gdx.graphics.Color;
+import com.google.common.base.Preconditions;
 
 public class Util {
 
@@ -15,5 +16,50 @@ public class Util {
         float g = (hex & 0x0000FF00L) >> 8;
         float b = (hex & 0x000000FFL);
         return new Color(r / 255F, g / 255F, b / 255F, a / 255F);
+    }
+
+    /**
+     * @param min
+     *     The minimum value to return (inclusive)
+     * @param val
+     *     The value to verify is between {@code min} and {@code max}
+     * @param max
+     *     The maximum value to return (inclusive)
+     *
+     * @return {@code val} if between {@code min} and {@code max}, if not return {@code min} or {@code max} respectivly
+     */
+    public static <T extends Comparable<T>> T between(T min, T val, T max) {
+        Preconditions.checkNotNull(min, "None of the parameters can be null");
+        Preconditions.checkNotNull(val, "None of the parameters can be null");
+        Preconditions.checkNotNull(max, "None of the parameters can be null");
+        Preconditions.checkArgument(min.compareTo(max) <= 0,
+                                    "Minimum argument must be less than or equal to the maximum argument");
+        if (val.compareTo(min) < 0) {
+            return min;
+        }
+        else if (val.compareTo(max) > 0) {
+            return max;
+        }
+        return val;
+    }
+
+    /**
+     * @param min
+     *     The minimum value to check (inclusive)
+     * @param val
+     *     The value to verify is between {@code min} and {@code max}
+     * @param max
+     *     The maximum value to check (exclusive)
+     *
+     * @return if {@code val} is between {@code min} (inclusive) and {@code max} (exclusive)
+     */
+    public static <T extends Comparable<T>> boolean isBetween(T min, T val, T max) {
+        Preconditions.checkNotNull(min, "None of the parameters can be null");
+        Preconditions.checkNotNull(val, "None of the parameters can be null");
+        Preconditions.checkNotNull(max, "None of the parameters can be null");
+        Preconditions.checkArgument(min.compareTo(max) <= 0,
+                                    "Minimum argument must be less than or equal to the maximum argument");
+        if (val.compareTo(min) < 0) { return false; }
+        else { return val.compareTo(max) < 0; }
     }
 }
---
+++ core/src/no/erlyberly/bootlegterraria/util/UtilTest.java
@@ -0,0 +1,37 @@
+package no.erlyberly.bootlegterraria.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    @Test
+    void between() {
+        assertEquals(new Integer(0), Util.between(0, -1, 1)); //check less than min
+        assertEquals(new Integer(1), Util.between(1, 0, 2)); //check between
+        assertEquals(new Integer(1), Util.between(0, 2, 1)); //check max
+        assertEquals(new Integer(0), Util.between(0, 0, 0)); //check max == min
+
+        //make sure min must less than or equal to max
+        assertThrows(IllegalArgumentException.class, () -> Util.between(2, 0, 0));
+    }
+
+    @Test
+    void isBetween() {
+        assertTrue(Util.isBetween(0, 1, 2));
+        assertFalse(Util.isBetween(0, -1, 2));
+        assertFalse(Util.isBetween(0, 3, 2));
+
+        assertTrue(Util.isBetween(0, 0, 2));
+        assertFalse(Util.isBetween(0, 2, 2));
+
+        int width = 16;
+        int x;
+        for (int i = 0; i < 1000; i++) {
+            x = (int) (Math.random() * 16);
+            assertEquals((x < 0 || x >= width), !Util.isBetween(0, x, width), "x=" + x);
+        }
+
+    }
+}
