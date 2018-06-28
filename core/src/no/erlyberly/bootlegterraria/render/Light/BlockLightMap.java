7ddc0d387541432543e5d9a69023dcd61f5736e0
Karl Henrik Elg <klink100@gmail.com>
2018-06-28 21:58:12 +0200

    FIX OBO error with skylight

--- core/src/no/erlyberly/bootlegterraria/render/Light/BlockLightMap.java
+++ core/src/no/erlyberly/bootlegterraria/render/Light/BlockLightMap.java
@@ -175,7 +175,7 @@ public class BlockLightMap implements LightMap {
                     if (blockX + 1 < this.skylight.length && this.skylight[blockX + 1] < y) {
                         addSkylight(blockX + 1, y);
                     }
-                    if (blockX - 1 > 0 && this.skylight[blockX - 1] < y) {
+                    if (blockX - 1 >= 0 && this.skylight[blockX - 1] < y) {
                         addSkylight(blockX - 1, y);
                     }
                 }
